3a4ce7efb3413ab6855a48f410039a73726548ea
Hitesh Sankhat <hitesh.sankhat@zeuslearning.com>
2022-12-20 03:03:35 +0530

    update algo logic

--- js/script.js
+++ js/script.js
@@ -1,107 +1,106 @@
-const getBtn = document.getElementById("get-btn");
-const startBtn = document.getElementById("start-btn");
-const clearBtn = document.getElementById("clear-btn");
-const countElement = document.getElementById("total_element");
-const startElement = document.getElementById("start");
-const endElement = document.getElementById("end");
-const arrayContainer = document.getElementById("array-container");
-const animationDuration = 1000;
-const timeOutArray = [];
+MAX_VALUE = 100;
+SPEED = 100;
 
-const randomNumber = (start, end) => {
-  return Math.floor(Math.random() * (end - start)) + start;
-};
-
-const arrayOfRandom = (count, start, end) => {
-  const array = [];
-  for (let i = 0; i < count; i++) {
-    array.push(randomNumber(start, end));
+class DrawArray {
+  constructor(maxValue) {
+    this.barSize = 5;
+    [this.ctx, this.rect] = setUpCanvas();
+    this.width = this.rect.width;
+    this.height = this.rect.height - 60;
+    this.maxValue = maxValue;
   }
-  return array;
-};
 
-const swapWithAnimate = async (a, b) => {
-  const rect1 = a.getBoundingClientRect();
-  const react2 = b.getBoundingClientRect();
-  const moveVal = Math.abs(react2.x - rect1.x);
-
-  a.animate(
-    [
-      { transform: "translate(0, -50px)", backgroundColor: "red" },
-      { transform: `translate(${moveVal}px, -50px)`, backgroundColor: "red" },
-      { transform: `translate(${moveVal}px, 0)`, backgroundColor: "red" },
-    ],
-    {
-      duration: animationDuration,
-    }
-  );
-
-  b.animate(
-    [
-      { transform: "translate(0, 50px)", backgroundColor: "red" },
-      { transform: `translate(-${moveVal}px, 50px)`, backgroundColor: "red" },
-      { transform: `translate(-${moveVal}px, 0)`, backgroundColor: "red" },
-    ],
-    {
-      duration: animationDuration,
-    }
-  );
-};
-
-const bubbleSort = async (e) => {
-  clearAllTimeOuts();
-  const array = document.querySelectorAll(".element");
-  const numbers = [];
-  array.forEach((el) => numbers.push(parseInt(el.textContent)));
-  let timeOut = 0;
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = 0; j < array.length - i - 1; j++) {
-      const node1 = array[j];
-      const node2 = array[j + 1];
-      if (numbers[j] > numbers[j + 1]) {
-        [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]];
-        const clearTimeOutObj = setTimeout(() => {
-          swapWithAnimate(node1, node2);
-          const numberTimeOut = setTimeout(() => {
-            [node1.textContent, node2.textContent] = [
-              node2.textContent,
-              node1.textContent,
-            ];
-            clearTimeout(numberTimeOut);
-          }, animationDuration);
-          timeOutArray.push(numberTimeOut);
-          clearTimeout(clearTimeOutObj);
-        }, timeOut);
-        timeOutArray.push(clearTimeOutObj);
-        timeOut += animationDuration + 500;
+  draw(array) {
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const n = array.length;
+    const startX = Math.round((this.width - n * (2 * this.barSize)) / 2);
+    drawLine(
+      this.ctx,
+      startX - 5,
+      0,
+      n * (2 * this.barSize) + 5,
+      this.barSize,
+      "black"
+    );
+    array.forEach((element, index) => {
+      const [x, y] = [
+        startX + index * (2 * this.barSize),
+        Math.floor(element * (this.height / this.maxValue)),
+      ];
+      drawLine(this.ctx, x, this.barSize, 5, y);
+    });
+  }
+}
+function getUserInputData() {
+  const startButton = document.getElementById("start");
+  startButton.addEventListener("click", start);
+}
+function generateArray(numberOfElement) {
+  const numberArray = new Array(numberOfElement);
+  for (let i = 0; i < numberOfElement; i++) {
+    numberArray[i] = Math.round(Math.random() * MAX_VALUE);
+  }
+  return numberArray;
+}
+function setUpCanvas() {
+  const canvas = document.getElementById("canvas");
+  const dpr = window.devicePixelRatio;
+  canvas.style.width = `${Math.floor(window.innerWidth * 0.99)}px`;
+  canvas.style.height = `${Math.floor(
+    (window.innerHeight - canvas.offsetTop) * 0.99
+  )}px`;
+  const rect = canvas.getBoundingClientRect();
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.height * dpr;
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.scale(dpr, dpr);
+  return [ctx, rect];
+}
+function drawLine(ctx, x, y, width, height, color = "green") {
+  ctx.beginPath();
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+  ctx.closePath();
+}
+function bubbleSort(array, drawArray) {
+  const arrCopy = [...array];
+  let swapped;
+  function sort() {
+    swapped = false;
+    for (let i = 0; i < arrCopy.length - 1; i++) {
+      if (arrCopy[i] > arrCopy[i + 1]) {
+        [arrCopy[i], arrCopy[i + 1]] = [arrCopy[i + 1], arrCopy[i]];
+        swapped = true;
       }
+      drawArray(arrCopy);
+    }
+
+    if (swapped) {
+      var t = setTimeout(() => {
+        requestAnimationFrame(sort);
+        clearTimeout(t);
+      }, SPEED);
+    } else {
+      // console.log(arrCopy);
     }
   }
-};
 
-const clearAllTimeOuts = () => {
-  timeOutArray.forEach((timeOut) => {
-    clearTimeout(timeOut);
-  });
-};
+  sort();
+  // console.log(arrCopy);
+}
+function start() {
+  const numberOfElementInput = document.getElementById("number_of_elements");
+  const numberOfElement = Number(numberOfElementInput.value);
 
-getBtn.addEventListener("click", () => {
-  const count = countElement.value;
-  const start = startElement.value;
-  const end = endElement.value;
-  if (count > 20 || count < 1) {
-    alert("please enter number in range 1 to 20");
+  if (isNaN(numberOfElement) || numberOfElement <= 0) {
+    alert("Invalid input. Please enter a positive number.");
     return;
   }
-  const array = arrayOfRandom(+count, +start, +end);
-  arrayContainer.textContent = "";
+  const arr = generateArray(numberOfElement);
+  const d = new DrawArray(Math.max(...arr));
+  d.draw(arr);
+  bubbleSort(arr, d.draw.bind(d));
+}
 
-  array.forEach((el) => {
-    const html = `<div class="element">${el}</div>`;
-    arrayContainer.insertAdjacentHTML("beforeend", html);
-  });
-});
-
-clearBtn.addEventListener("click", clearAllTimeOuts);
-
-startBtn.addEventListener("click", bubbleSort);
+getUserInputData();
